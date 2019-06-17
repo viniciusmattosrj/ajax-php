33f38234c4bacfae10dfe8b46dbb51705afa93a7
Vinicius Mattos <vinimattos.rj@gmail.com>
2019-06-17 08:32:23 -0300

    20190617 - Correção nome da BD.

--- app/models/Connection.php
+++ app/models/Connection.php
@@ -8,7 +8,7 @@ class Connection{
 
     public static function connect(){
 
-        $pdo = new PDO("mysql:host=10.11.0.6;dbname=users", "root", "A123456");
+        $pdo = new PDO("mysql:host=10.11.0.6;dbname=php_ajax", "root", "A123456");
 
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
