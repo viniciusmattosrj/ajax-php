580b8b550b28af7db7b67bdfe49682f4389b5091
Vinicius Mattos <vinimattos.rj@gmail.com>
2019-06-17 07:46:05 -0300

    20190617 - Atualizando .sql

--- _sql/sql.sql
+++ _sql/sql.sql
@@ -14,3 +14,16 @@ desc users
 
 -- Apagando tabela
 DROP TABLE users;
+
+-- Indicando qual tabela usar
+use php_ajax;
+
+-- Populando com dados
+INSERT INTO users (name) values('Vinicius Mattos');
+INSERT INTO users (name) values('Carlos Roberto');
+INSERT INTO users (name) values('Thais Souza');
+INSERT INTO users (name) values('Jean Silva');
+INSERT INTO users (name) values('Monica Silva');
+
+-- Simples consulta database
+SELECT * FROM users;
