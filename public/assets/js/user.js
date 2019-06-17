10c75b9b41bb461670954f97833bf79bc87b3801
Vinicius Mattos <vinimattos.rj@gmail.com>
2019-06-16 21:43:35 -0300

    20190616 - Correções diversas.

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -1,16 +1,16 @@
-window.onload = function(){
+window.onload = function () {
 
     var btn_users = document.querySelector("#btn-users");
 
     var xhttp = new XMLHttpRequest();
 
-    btn_users.onclick = function () { 
+    btn_users.onclick = function () {
 
-        xhttp.onreadystatechange = function(){
+        xhttp.onreadystatechange = function () {
 
             console.log(xhttp.readyState);
 
-            if (this.readyState = 4 && this.status == 200) { 
+            if (this.readyState = 4 && this.status == 200) {
                 console.log(this.responseText);
             }
         }
@@ -18,11 +18,11 @@ window.onload = function(){
         console.log('veio depois do onready');
 
         /* 
-            1. O true signigica que estou trabalhando com uma requisição assincrona 
-            2. Ele é um callback da function declarado na linha 7
+            1. O true significa que estou trabalhando com uma requisição assíncrona 
+            2. E essa requição é um callback da function declarada na linha 7
         */
         xhttp.open('GET', 'ajax/user.php', true);
-    
+
         xhttp.send();
     }
 
