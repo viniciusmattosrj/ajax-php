29f1516ec5bafcd6d556fa1abdffb905230da220
Vinicius Mattos <vinimattos.rj@gmail.com>
2019-06-28 15:21:07 -0300

    20190628 - Refatorando a estrutura ajax.

---
+++ public/assets/js/xhttp.js
@@ -0,0 +1,32 @@
+var xhttp = new XMLHttpRequest;
+
+function xmlHttpGet(url, callback, parameters='') {
+
+    xhttp.onreadystatechange = callback;
+
+    xhttp.open('GET',url+'.php'+parameters, true);
+
+    xhttp.send();
+}
+
+function xmlHttpPost() {
+    xhttp.onreadystatechange = callback;
+    xhttp.open('POST',url+'.php'+parameters, true);
+    xhttp.send(parameters);
+}
+
+function beforeSend(callback) {
+    if (xhttp.readyState < 4) {
+        callback();
+    }
+}
+
+function success(callback) {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+        callback();
+    }
+}
+
+function error(callback) {
+    xhttp.onerror = callback;
+}
