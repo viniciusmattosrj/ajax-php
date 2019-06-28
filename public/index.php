648620fe863f06858d4c8c22491499eb041040b3
Vinicius Mattos <vinimattos.rj@gmail.com>
2019-06-28 13:10:17 -0300

    20190628 - Melhorias e update.

--- public/index.php
+++ public/index.php
@@ -5,14 +5,19 @@
     <meta charset="UTF-8">
     <title>Ajax</title>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 </head>
 
 <body>
     <script src="assets/js/user.js"></script>
 
-    <button id="btn-users">Users</button>
+    <div class="container">
+        <div class="row">
+            <button id="btn-users" class="btn btn-primary btn-xs">Users</button>
+            <div id="div-users"></div>
+        </div>
+    </div>
 
-    <div id="div-users"></div>
 </body>
 
 </html>
